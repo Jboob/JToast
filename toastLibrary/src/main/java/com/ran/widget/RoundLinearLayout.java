35d0bdb3e9014d6bd003a2a91b1156bf413c174b
jboob <tyyq9851@sina.com>
2018-11-18 16:29:42 +0800

    update RoundLinerLayout

--- toastLibrary/src/main/java/com/ran/widget/RoundLinearLayout.java
+++ toastLibrary/src/main/java/com/ran/widget/RoundLinearLayout.java
@@ -2,11 +2,13 @@ package com.ran.widget;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.Color;
 import android.graphics.drawable.GradientDrawable;
 import android.os.Build;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.View;
 import android.widget.LinearLayout;
 
 import com.ran.toast.R;
@@ -31,16 +33,19 @@ public class RoundLinearLayout extends LinearLayout {
 
     public RoundLinearLayout(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
-
+        init(context,attrs);
     }
 
     public RoundLinearLayout(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        Log.d(TAG, "drawRoundDrawable: "+cornesRadius);
+        init(context,attrs);
+    }
+
+    public void init(Context context, @Nullable AttributeSet attrs){
         TypedArray arr = context.obtainStyledAttributes(attrs, R.styleable.RoundLayout);
-        this.cornesRadius = arr.getFloat(R.styleable.RoundLayout_radius, 0);
+        this.cornesRadius = (float) arr.getInt(R.styleable.RoundLayout_radius, 0);
         if (cornesRadius == 0) {
-            float topLeftRadius = arr.getFloat(R.styleable.RoundLayout_topLeftRadius, 0);
+            float topLeftRadius = (float) arr.getInt(R.styleable.RoundLayout_topLeftRadius, 0);
             float topRightRadius = arr.getFloat(R.styleable.RoundLayout_topRightRadius, 0);
             float bottomLeftRadius = arr.getFloat(R.styleable.RoundLayout_bottomLeftRadius, 0);
             float bottomRightRadius = arr.getFloat(R.styleable.RoundLayout_bottomRightRadius, 0);
@@ -48,6 +53,7 @@ public class RoundLinearLayout extends LinearLayout {
                     bottomLeftRadius, bottomLeftRadius, bottomRightRadius, bottomRightRadius};
         }
         drawRoundDrawable();
+        arr.recycle();
     }
 
     public void setRadius(float radius){
@@ -55,17 +61,19 @@ public class RoundLinearLayout extends LinearLayout {
     }
 
     private void drawRoundDrawable() {
-        Log.d(TAG, "drawRoundDrawable: "+cornesRadius);
-
         GradientDrawable drawable = null;
+
         if (cornesRadius != 0) {
             drawable = new GradientDrawable();
+            drawable.setColor(Color.GRAY);
             drawable.setCornerRadius(cornesRadius);
-        } else if (null != radii) {
+            drawable.setGradientType(GradientDrawable.RECTANGLE);
+        }else if (null != radii){
             drawable = new GradientDrawable();
+            drawable.setColor(Color.GRAY);
             drawable.setCornerRadii(radii);
+            drawable.setGradientType(GradientDrawable.RECTANGLE);
         }
-
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
             if (null != drawable)
                 setBackground(drawable);
